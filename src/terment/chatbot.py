c403b93a40af0d4f65bf146eeb79631c569a6880
Kumar Sarthak <kumarsarthak0712@gmail.com>
2026-07-11 13:17:24 +0530

    refactor: seperated cli logic from main chatbot and updated script entry point

---
+++ src/terment/chatbot.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+from rich.console import Console
+
+from terment.providers import Provider
+
+console = Console()
+
+
+class Chatbot:
+    def __init__(
+        self, model: str, provider: Provider, system_prompt: str | None
+    ) -> None:
+        if provider.api_key == "":
+            raise ValueError("Api Key not found for the selected provider.")
+        self.messages: list[ChatCompletionMessageParam] = []
+        self.client = OpenAI(api_key=provider.api_key, base_url=provider.base_url)
+        self.model = model
+        self.system_prompt = system_prompt
+        if self.system_prompt is not None:
+            self.messages.append({"role": "system", "content": self.system_prompt})
+
+    def _generate_response(self, prompt: str):
+        self.messages.append({"role": "user", "content": prompt})
+        stream = self.client.chat.completions.create(
+            messages=self.messages,
+            model=self.model,
+            stream=True,
+            reasoning_effort="none",
+        )
+        final_response_list = []
+        for chunks in stream:
+            content = chunks.choices[0].delta.content
+            if content is not None:
+                final_response_list.append(content)
+                final_response = "".join(final_response_list)
+                yield final_response
+        self.messages.append({"role": "assistant", "content": final_response})
--- src/terment/cli.py
+++ src/terment/cli.py
@@ -1,53 +1,23 @@
-import time
-import json
-from openai import OpenAI
-from openai import APIConnectionError
-from openai.types.chat import ChatCompletionMessageParam
-from dotenv import load_dotenv
-from rich.console import Console
-from rich.markdown import Markdown
+from time import strftime
+from json import dump
 from rich.panel import Panel
-from rich.live import Live
+from rich.markdown import Markdown
 from collections.abc import Generator
+from rich.live import Live
+from rich.console import Console
 from pathlib import Path
+from openai import APIConnectionError
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+
 
 from terment.config_manager import get_selected_provider
-from terment.providers import Provider
+from terment.chatbot import Chatbot
 
-load_dotenv()
 console = Console()
 
 
-class Chatbot:
-    def __init__(
-        self, model: str, provider: Provider, system_prompt: str | None
-    ) -> None:
-        if provider.api_key == "":
-            raise ValueError("Api Key not found for the selected provider.")
-        self.messages: list[ChatCompletionMessageParam] = []
-        self.client = OpenAI(api_key=provider.api_key, base_url=provider.base_url)
-        self.model = model
-        self.system_prompt = system_prompt
-        if self.system_prompt is not None:
-            self.messages.append({"role": "system", "content": self.system_prompt})
-
-    def _generate_response(self, prompt: str):
-        self.messages.append({"role": "user", "content": prompt})
-        stream = self.client.chat.completions.create(
-            messages=self.messages,
-            model=self.model,
-            stream=True,
-            reasoning_effort="none",
-        )
-        final_response_list = []
-        for chunks in stream:
-            content = chunks.choices[0].delta.content
-            if content is not None:
-                final_response_list.append(content)
-                final_response = "".join(final_response_list)
-                yield final_response
-        self.messages.append({"role": "assistant", "content": final_response})
-
+class CliChatbot(Chatbot):
     def _render_message(self, streamed_response: Generator) -> None:
         panel = Panel(
             renderable="",
@@ -64,12 +34,12 @@ class Chatbot:
                 live.refresh()
 
     def _save_chat(self):
-        file_name = Path("sessions") / f"{time.strftime('%y%b%d%H%M%S')}"
+        file_name = Path("sessions") / f"{strftime('%y%b%d%H%M%S')}"
         Path.touch(file_name)
         with open(
             f"{file_name}.json", "w", encoding="utf-8"
         ) as conversation_history_file:
-            json.dump(self.messages, conversation_history_file, indent=4)
+            dump(self.messages, conversation_history_file, indent=4)
 
     def terminal_chat(self) -> None:
         while True:
@@ -92,7 +62,7 @@ selected_model = selected_provider_dict.get("model", None)
 selected_provider = selected_provider_dict.get("provider", None)
 
 if selected_provider is not None and selected_model is not None:
-    terment = Chatbot(
+    terment = CliChatbot(
         model=selected_model,
         provider=selected_provider,
         system_prompt="You are a helpful agent which uses dry humour with a cozy tone .",
@@ -104,7 +74,3 @@ def main():
         terment.terminal_chat()
     except APIConnectionError:
         print("Failed to Connect with the servers")
-
-
-if __name__ == "__main__":
-    main()
